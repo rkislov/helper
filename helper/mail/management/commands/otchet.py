06ba4b8df8641462ddbee9d7403cf1d037ded4d1
Roman Kislov <roman@kislovs.ru>
2023-12-15 20:30:37 +0300

    otchet3

--- helper/mail/management/commands/otchet.py
+++ helper/mail/management/commands/otchet.py
@@ -3,11 +3,13 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-connect = psycopg2.connect(host=os.getenv("DBE_HOST"), user=os.getenv("DBE_USER"), password=os.getenv("DBE_PASSWORD"), dbname=os.getenv("DBE_NAME"), port=os.getenv("DBE_PORT"))
 
-cursor = connect.cursor()
-cursor.execute("SELECT * FROM public.ticket ORDER BY id DESC LIMIT 10")
-row = cursor.fetchone()
-cursor.close()
-connect.close()
-print(row)
+class Command(BaseCommand):
+    connect = psycopg2.connect(host=os.getenv("DBE_HOST"), user=os.getenv("DBE_USER"), password=os.getenv("DBE_PASSWORD"), dbname=os.getenv("DBE_NAME"), port=os.getenv("DBE_PORT"))
+
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM public.ticket ORDER BY id DESC LIMIT 10")
+    row = cursor.fetchone()
+    cursor.close()
+    connect.close()
+    print(row)
