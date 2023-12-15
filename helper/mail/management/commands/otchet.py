d07fbaf8bf81bc60d6480e40bc7c6964200ec96c
Roman Kislov <roman@kislovs.ru>
2023-12-15 20:29:35 +0300

    otchet2

--- helper/mail/management/commands/otchet.py
+++ helper/mail/management/commands/otchet.py
@@ -6,6 +6,7 @@ import os
 connect = psycopg2.connect(host=os.getenv("DBE_HOST"), user=os.getenv("DBE_USER"), password=os.getenv("DBE_PASSWORD"), dbname=os.getenv("DBE_NAME"), port=os.getenv("DBE_PORT"))
 
 cursor = connect.cursor()
+cursor.execute("SELECT * FROM public.ticket ORDER BY id DESC LIMIT 10")
 row = cursor.fetchone()
 cursor.close()
 connect.close()
