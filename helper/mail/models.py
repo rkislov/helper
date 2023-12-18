0c1001605db863d37368461c4b00ed610e7467e4
Roman Kislov <roman@kislovs.ru>
2023-12-18 17:04:51 +0500

    region model

--- helper/mail/models.py
+++ helper/mail/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Region(models.Model):
+    region_number = models.CharField(max_length=2)
+    region_admin_email = models.EmailField()
+    build_otchet = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Регион"
+        verbose_name_plural = "Регионы"
+
+    def __str__(self):
+        return self.region_number
