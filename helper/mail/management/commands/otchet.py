1723489e184bab75e0783e9b9ddf95205f27cc8b
Roman Kislov <roman@kislovs.ru>
2023-12-18 16:27:07 +0500

    otchet18

--- helper/mail/management/commands/otchet.py
+++ helper/mail/management/commands/otchet.py
@@ -7,6 +7,9 @@ import os
 import csv
 import datetime
 
+
+def tuple_to_list(t):
+    return [list(x) for x in t]
 class Command(BaseCommand):
     help = "проверят наличие новых заявок в ОТРС"
 
@@ -48,12 +51,13 @@ class Command(BaseCommand):
 
         print(row[0])
         print(row[1])
+
+        print(tuple_to_list(row))
         # with open(os.path.join(django_settings.STATIC_ROOT,f'{filename}'), 'w') as f:
         #     csv_writer = csv.writer(f)
         #     csv_writer.writerow(fields)
         #     for ro in row:
         #         csv_writer.writerow(ro[2], ro[0], ro[3], ro[5], ro[7], ro[7], ro[6])
-
 
         cursor.close()
         connect.close()
