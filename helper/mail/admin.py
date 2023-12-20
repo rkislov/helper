a6f8039cbb3aa5794c00eef4735d5b79c7c6c66a
Roman Kislov <roman@kislovs.ru>
2023-12-20 16:23:43 +0500

    button

--- helper/mail/admin.py
+++ helper/mail/admin.py
@@ -3,3 +3,12 @@ from .models import Region
 
 
 admin.site.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'region_number', 'region_admin_email', 'build_otchet']
+    search_fields = ['id', 'region_number', 'region_admin_email']
+    list_filter = ['build_otchet']
+
+
+    def match_update(self, request, *args):
+        from django.core.management import call_command
+        call_command("otchet")
