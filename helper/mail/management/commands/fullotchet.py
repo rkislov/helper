c65d2eea16e53d24929af4b1d4f3ddf05fef355f
Roman Kislov <roman@kislovs.ru>
2024-01-30 21:21:49 +0300

    debug 7

--- helper/mail/management/commands/fullotchet.py
+++ helper/mail/management/commands/fullotchet.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from mail.utils import create_fullotchet
 
-from helper.mail.models import Topic
+from mail.models import Topic
 
 
 class Command(BaseCommand):
