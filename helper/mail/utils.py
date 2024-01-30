604a6c3786ede2209764bc385bf5d9765bdb3b60
Roman Kislov <roman@kislovs.ru>
2024-01-30 22:11:30 +0300

    debug 10

--- helper/mail/utils.py
+++ helper/mail/utils.py
@@ -102,18 +102,12 @@ E-mail:  supportcp@cloud.rt.ru
 
 
 def create_fullotchet(topic):
-   # region = Region.objects.filter(region_number=region_number).first()
+
 
     connect = psycopg2.connect(host=os.getenv("DBE_HOST"), user=os.getenv("DBE_USER"),
                                password=os.getenv("DBE_PASSWORD"), dbname=os.getenv("DBE_NAME"),
                                port=os.getenv("DBE_PORT"))
 
-#     filename = f"region-{region_number}-{datetime.date.today().isoformat()}.xlsx"
-#     subject = f"region-{region_number}-{datetime.date.today().isoformat()}"
-#
-#
-    # topic = Topic.objects.filter(slug='all').first()
-    # alltopics = Topic.objects.exclude(slug='all').all()
     filename = f"{topic.slug}-{datetime.date.today().isoformat()}.xlsx"
     subject = f"{topic.slug}-{datetime.date.today().isoformat()}"
 
@@ -127,8 +121,6 @@ def create_fullotchet(topic):
 E-mail:  supportcp@cloud.rt.ru
      """
 
-#     search_region = f"'%{region.region_number}%'"
-#     print(search_region)
 
     cursor = connect.cursor()
     sql = f"""
@@ -140,7 +132,6 @@ E-mail:  supportcp@cloud.rt.ru
     rows = cursor.fetchall()
     print(rows[0])
     print("Total rows are:  ", len(rows))
-    #print("Row 0: ", rows[0])
 
 
     fields = ['№ п/п', 'Дата поступления', 'Время поступления', 'Номер обращения', 'Заявитель (фамилия и инициалы)',
@@ -150,20 +141,29 @@ E-mail:  supportcp@cloud.rt.ru
               'Необходимость модификации СПО', 'Номер листа внимания', 'дата закрытия', 'Время закрытия',
               'Общее время обработки', 'Канал поступления']
 
+    fields2 =['Статус', 'Количество']
     path = os.path.relpath(os.path.join(django_settings.STATIC_ROOT, f'{filename}'))
     workbook = xlsxwriter.Workbook(path)
     worksheet = workbook.add_worksheet()
+    worksheet2 = workbook.add_worksheet()
     format1 = workbook.add_format({'bg_color': '#D9D9D9', 'bold': True})
     row = 0
     col = 0
+    row2 = 0
+    col2 = 0
     for f in fields:
         worksheet.write(row, col, f, format1)
         col += 1
     row += 1
     col = 0
+    for f in fields2:
+        worksheet.write(row2, col2, f, format1)
+        col2 += 1
+    row2 += 1
+    col2 = 0
+    dict = {}
     if topic.slug == 'all':
         for ro in rows:
-            date_time = f"{ro[5]}"
             worksheet.write(row, col, ro[0])
             worksheet.write(row, col + 1, ro[1])
             worksheet.write(row, col + 2, ro[2])
@@ -184,14 +184,15 @@ E-mail:  supportcp@cloud.rt.ru
             worksheet.write(row, col + 17, ro[17])
             worksheet.write(row, col + 18, ro[18])
             worksheet.write(row, col + 19, ro[19])
-            # if ro[8] != None:
-            #     worksheet.write(row, col + 8, ro[8].strip())
             row += 1
             col = 0
+            if ro[12] not in dict:
+                dict[ro[12]] = 1
+            else:
+                dict[ro[12]] += 1
     else:
         for ro in rows:
             if ro[10] == topic.name:
-                date_time = f"{ro[5]}"
                 worksheet.write(row, col, ro[0])
                 worksheet.write(row, col + 1, ro[1])
                 worksheet.write(row, col + 2, ro[2])
@@ -212,16 +213,26 @@ E-mail:  supportcp@cloud.rt.ru
                 worksheet.write(row, col + 17, ro[17])
                 worksheet.write(row, col + 18, ro[18])
                 worksheet.write(row, col + 19, ro[19])
-                # if ro[8] != None:
-                #     worksheet.write(row, col + 8, ro[8].strip())
                 row += 1
                 col = 0
+                if ro[12] not in dict:
+                    dict[ro[12]] = 1
+                else:
+                    dict[ro[12]] += 1
     worksheet.autofit()
     worksheet.freeze_panes(1, 0)
     worksheet.autofilter(0, 0, 1000, 20)
     worksheet.set_default_row(50)
+    for key, value in dict:
+        worksheet2.write(row2, col2, key)
+        worksheet2.write(row2, col2 + 1, value)
+        row2 += 1
+        col2 = 0
+    worksheet.autofit()
+    worksheet.freeze_panes(1, 0)
+    worksheet.autofilter(0, 0, 1000, 2)
+    worksheet.set_default_row(50)
     workbook.close()
-    #path = make_file(filename, rows)
 
     emails = []
     iteremails = topic.recivers.all()
@@ -230,86 +241,8 @@ E-mail:  supportcp@cloud.rt.ru
         emails.append(email.email)
     print(emails)
     print(filename)
-    #path = os.path.relpath(os.path.join(django_settings.STATIC_ROOT, f'{filename}'))
-    #send_otchet_email_task.delay([region.region_admin_email], subject, 'post@cifro.tech', message, filename)
     send_otchet_email_task.delay(emails, subject, 'post@cifro.tech', message, filename)
 
-    # for top in alltopics:
-    #     print("ready :", filename)
-    #     df = pd.read_excel(path)
-    #     for service in top.services:
-    #         message = f"""Добрый день.
-    #             Во вложении полный отчет по сервису {top.name}  на {datetime.date.today().isoformat()}
-    #
-    #            Служба поддержки ЦП
-    #            Телефон: 8-800-301-23-39
-    #            E-mail:  supportcp@cloud.rt.ru
-    #                 """
-    #         df_rows = df[df['Наименование подсистемы'] == service.name]
-    #         newrows = []
-    #         newemails = []
-    #         iteremails = topic.recivers.all()
-    #         for email in iteremails:
-    #             emails.append(email.email)
-    #
-    #         newfilename = f"{service.name}-{datetime.date.today().isoformat()}.xlsx"
-    #         newsubject = f"{service.name}-{datetime.date.today()}"
-    #         for row in df_rows.itterows():
-    #             newrows.append(row)
-    #
-    #             fields = ['№ п/п', 'Дата поступления', 'Время поступления', 'Номер обращения',
-    #                       'Заявитель (фамилия и инициалы)',
-    #                       'Название субъекта РФ', 'Номер СТД (КСА)', 'Текст обращения', 'Классификация обращения',
-    #                       'Приоритет обращения',
-    #                       'Наименование подсистемы', 'Исполнитель обращения', 'Текущий статус',
-    #                       'Описание оказанной консультации',
-    #                       'Необходимость модификации СПО', 'Номер листа внимания', 'дата закрытия', 'Время закрытия',
-    #                       'Общее время обработки', 'Канал поступления']
-    #
-    #             newpath = os.path.relpath(os.path.join(django_settings.STATIC_ROOT, f'{newfilename}'))
-    #             workbook = xlsxwriter.Workbook(path)
-    #             worksheet = workbook.add_worksheet()
-    #             format1 = workbook.add_format({'bg_color': '#D9D9D9', 'bold': True})
-    #             row = 0
-    #             col = 0
-    #             for f in fields:
-    #                 worksheet.write(row, col, f, format1)
-    #                 col += 1
-    #             row += 1
-    #             col = 0
-    #             for ro in newrows:
-    #                 date_time = f"{ro[5]}"
-    #                 worksheet.write(row, col, ro[0])
-    #                 worksheet.write(row, col + 1, ro[1])
-    #                 worksheet.write(row, col + 2, ro[2])
-    #                 worksheet.write(row, col + 3, ro[3])
-    #                 worksheet.write(row, col + 4, ro[4])
-    #                 worksheet.write(row, col + 5, ro[5])
-    #                 worksheet.write(row, col + 6, ro[6])
-    #                 worksheet.write(row, col + 7, ro[7])
-    #                 worksheet.write(row, col + 8, ro[8])
-    #                 worksheet.write(row, col + 9, ro[9])
-    #                 worksheet.write(row, col + 10, ro[10])
-    #                 worksheet.write(row, col + 11, ro[11])
-    #                 worksheet.write(row, col + 12, ro[12])
-    #                 worksheet.write(row, col + 13, ro[13])
-    #                 worksheet.write(row, col + 14, ro[14])
-    #                 worksheet.write(row, col + 15, ro[15])
-    #                 worksheet.write(row, col + 16, ro[16])
-    #                 worksheet.write(row, col + 17, ro[17])
-    #                 worksheet.write(row, col + 18, ro[18])
-    #                 worksheet.write(row, col + 19, ro[19])
-    #                 # if ro[8] != None:
-    #                 #     worksheet.write(row, col + 8, ro[8].strip())
-    #                 row += 1
-    #                 col = 0
-    #             worksheet.autofit()
-    #             worksheet.freeze_panes(1, 0)
-    #             worksheet.autofilter(0, 0, 1000, 20)
-    #             worksheet.set_default_row(50)
-    #             workbook.close()
-    #             print(newemails)
-    #             print(newpath)
-    #             send_otchet_email_task.delay(newemails, newsubject, 'post@cifro.tech', message, newpath)
+
 
 
