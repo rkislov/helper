56f90f69220888d17c8c5280cb4aad25d48492ef
Roman Kislov <roman@kislovs.ru>
2024-02-07 09:42:41 +0300

    new sql query5

--- helper/mail/utils.py
+++ helper/mail/utils.py
@@ -120,7 +120,8 @@ E-mail:  supportcp@cloud.rt.ru
      """
 
     date = datetime.date.today().isoformat()
-    date_for_otchet = datetime.date.today() + datetime.timedelta(hours=18)
+    time = datetime.time(18, 00)
+    date_for_otchet = datetime.datetime.combine(date, time)
     print(date)
     print(date_for_otchet)
     cursor = connect.cursor()
