b1ce059981bbf78df44d05a5af85c4269e456817
Roman Kislov <roman@kislovs.ru>
2024-02-15 11:41:26 +0300

    todb11

--- helper/mail/utils.py
+++ helper/mail/utils.py
@@ -301,7 +301,7 @@ def todb():
     #array = []
     #array.append(ro)
     #df = pd.DataFrame(list(rows), columns=fields)
-    sqlstring = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST"):}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME_TIKETS")}'
+    sqlstring = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME_TIKETS")}'
     print(sqlstring)
     df = pd.DataFrame(ro)
     engine = create_engine(sqlstring)
